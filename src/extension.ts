500c5968807cebb5155da77f393fd20c4d8e09e8
Timo Emanuel Keber <dev@timokeber.com>
2022-12-18 22:35:59 +0100

    minor refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -13,42 +13,52 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dart-idx-generator.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('dart-idx-generator.generateFile', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from dart_idx_generator!');
 
-		// check if a workspace is open
-		if (!vscode.workspace.workspaceFolders) {
-			vscode.window.showErrorMessage('No workspace is open');
-			return;
-		}
+
+
 
 		// generate a dart file called index.dart with all the dart files in the current folder
+
 		// get the current folder
-		const currentFolder = vscode.workspace.workspaceFolders;
-
-		// get the files in the current folder
-		const files = vscode.workspace.findFiles('**/*.dart');
-
-		// create the index.dart file
-		const indexFile = vscode.Uri.file(`${currentFolder[0].uri.fsPath}/index.dart`);
-
-		// create the content of the index.dart file
-		let content = '';
-		files.then((files) => {
-			files.forEach((file) => {
-				content += `export '${file.fsPath.replace(currentFolder[0].uri.fsPath, '')}';\r\n`;
-			});
-
-			// write the content to the index.dart file
-			vscode.workspace.fs.writeFile(indexFile, Buffer.from(content));
-		});
+		generateIndexFile();
 	});
 
 
 	context.subscriptions.push(disposable);
 }
 
+function generateIndexFile() {
+
+	// check if a workspace is open
+	if (!vscode.workspace.workspaceFolders) {
+		vscode.window.showErrorMessage('No workspace containing folders is open');
+		return;
+	}
+
+	const currentFolder = vscode.workspace.workspaceFolders;
+
+	// get the files in the current folder
+	const files = vscode.workspace.findFiles('**/*.dart');
+
+	// create the index.dart file
+	const indexFile = vscode.Uri.file(`${currentFolder[0].uri.fsPath}/index.dart`);
+
+	// create the content of the index.dart file
+	let content = '';
+	files.then((files) => {
+		files.forEach((file) => {
+			content += `export '${file.fsPath.replace(currentFolder[0].uri.fsPath, '')}';\r\n`;
+		});
+
+		// write the content to the index.dart file
+		vscode.workspace.fs.writeFile(indexFile, Buffer.from(content));
+	});
+}
+
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
