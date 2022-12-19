db7c218a99add9608e2215c24c90a494eb81b90d
Timo Emanuel Keber <dev@timokeber.com>
2022-12-19 12:35:17 +0100

    extend commands, not using disposables

--- src/extension.ts
+++ src/extension.ts
@@ -1,67 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { generateIndexFile } from './generateIndexFile';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dart-idx-generator" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'dart-idx-generator.generateFile',
+			generateIndexFile
+		)
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dart-idx-generator.generateFile', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Generated file for current directory!');
-
-
-
-
-		// generate a dart file called index.dart with all the dart files in the current folder
-
-		// get the current folder
-		generateIndexFile();
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'dart-idx-generator.generateFileForFolder',
+			generateIndexFile
+		)
+	);
 
 
-	context.subscriptions.push(disposable);
-}
 
-function generateIndexFile() {
-
-	// check if a workspace is open
-	if (!vscode.workspace.workspaceFolders) {
-		vscode.window.showErrorMessage('No workspace containing folders is open');
-		return;
-	}
-
-	const currentFolder = vscode.workspace.workspaceFolders;
-
-	// get the files in the current folder
-	const files = vscode.workspace.findFiles('**/*.dart');
-
-	// create the index.dart file
-	const indexFile = vscode.Uri.file(`${currentFolder[0].uri.fsPath}/index.dart`);
-
-	// create the content of the index.dart file
-	let content = '';
-	files.then((files) => {
-		files.forEach((file) => {
-
-			//remove *\* from beginning from beginning of the string, since its treated as an escape character
-			const path =
-				file.fsPath.replace(currentFolder[0].uri.fsPath, '').replace('\\', '');
-
-			content += `export '${path}';\r\n`;
-		});
-
-		// write the content to the index.dart file
-		vscode.workspace.fs.writeFile(indexFile, Buffer.from(content));
-	});
 }
 
 // This method is called when your extension is deactivated
---
+++ src/generateIndexFile.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+
+/// Called when the user executes the command "dart-idx-generator.generateFile" from the command palette. 
+/// Generates an index.dart file in the current folder containing all dart files in the current folder
+export function generateIndexFile() {
+
+    // check if a workspace is open
+    if (!vscode.workspace.workspaceFolders) {
+        vscode.window.showErrorMessage('No workspace containing folders is open');
+        return;
+    }
+
+    const currentFolder = vscode.workspace.workspaceFolders;
+
+    // get the files in the current folder
+    const files = vscode.workspace.findFiles('**/*.dart');
+
+    // create the index.dart file
+    const indexFile = vscode.Uri.file(`${currentFolder[0].uri.fsPath}/index.dart`);
+
+    // create the content of the index.dart file
+    let content = '';
+    files.then((files) => {
+        files.forEach((file) => {
+
+            //remove *\* from beginning from beginning of the string, since its treated as an escape character
+            const path = file.fsPath.replace(currentFolder[0].uri.fsPath, '').replace('\\', '');
+
+            content += `export '${path}';\r\n`;
+        });
+
+        // write the content to the index.dart file
+        vscode.workspace.fs.writeFile(indexFile, Buffer.from(content));
+    });
+}
