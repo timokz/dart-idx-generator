5b83ebbe3b0f3babfcb05252eaf568959d8db2d8
Timo Emanuel Keber <dev@timokeber.com>
2023-09-23 18:14:09 +0200

    default setup with printstmt

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,14 +3,19 @@ import * as path from "path";
 import { runTests } from "@vscode/test-electron";
 
 async function main() {
+  console.log("runTests called");
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
+    console.log("extensionDevelopmentPath", extensionDevelopmentPath);
+
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+
+    console.log("extensionTestsPath", extensionTestsPath);
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
